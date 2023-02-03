3e3241924406185c97bb341cf7be98d7837d5e8d
aruugah87 <69429749+aruugah87@users.noreply.github.com>
2023-02-03 10:55:39 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.txtbox3 = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Location = new System.Drawing.Point(156, 76);
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(225, 31);
+            this.txtbox1.TabIndex = 0;
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.Location = new System.Drawing.Point(156, 127);
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.Size = new System.Drawing.Size(225, 31);
+            this.txtbox2.TabIndex = 1;
+            // 
+            // txtbox3
+            // 
+            this.txtbox3.Location = new System.Drawing.Point(156, 185);
+            this.txtbox3.Name = "txtbox3";
+            this.txtbox3.ReadOnly = true;
+            this.txtbox3.Size = new System.Drawing.Size(225, 31);
+            this.txtbox3.TabIndex = 2;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(132, 266);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(118, 57);
+            this.calculate.TabIndex = 3;
+            this.calculate.Text = "calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(301, 266);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(112, 57);
+            this.Reset.TabIndex = 4;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "txtbox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "txtbox2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.txtbox3);
+            this.Controls.Add(this.txtbox2);
+            this.Controls.Add(this.txtbox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.TextBox txtbox3;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1
+{
+    public partial class Form1 : Form
+    {
+        private object txtResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToInt32(txtbox1.Text);
+            var num2 = Convert.ToInt32(txtbox2.Text);
+            var total = (num1 + num2).ToString();
+            txtbox3.Text = total.ToString();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            txtbox1.Text = string.Empty;
+            txtbox2.Text = string.Empty;
+            txtbox3.Text = string.Empty;
+        }
+    }
+}
